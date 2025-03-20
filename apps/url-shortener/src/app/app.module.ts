2462c024eefe73612aeb899eb6ca42036b775670
Fábio Albuquerque <albuquerquefabio@icloud.com>
2025-03-20 01:56:40 -0300

    Add configuration for global environment variables and MongoDB connection

--- apps/url-shortener/src/app/app.module.ts
+++ apps/url-shortener/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UrlModule } from '../api/url/url.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import env from '../config/dotenv/env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(env.mongodb.uri, {
+      dbName: env.mongodb.dbName,
+    }),
+    UrlModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
