94d20e82714d6036aaddd1a2cf615bc0b0c079aa
Fábio Albuquerque <albuquerquefabio@icloud.com>
2025-03-21 20:23:46 -0300

    Add User schema to MongoDB database configuration

--- apps/url-shortener/src/infra/mongodb/db.ts
+++ apps/url-shortener/src/infra/mongodb/db.ts
@@ -1,3 +1,7 @@
 import { Url, UrlSchema } from '../../api/url/entities/url.entity';
+import { User, UserSchema } from '../../api/users/entities/user.entity';
 
-export default [{ name: Url.name, schema: UrlSchema }];
+export default [
+  { name: Url.name, schema: UrlSchema },
+  { name: User.name, schema: UserSchema },
+];
