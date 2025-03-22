69d8d39a67dab328b24ad6740aa7084c1599f243
Fábio Albuquerque <albuquerquefabio@icloud.com>
2025-03-22 02:55:04 -0300

    Squashed commit of the following:
    commit 05764cf376be735b08a9f1302b967c4a5a0b86dc
    Author: Fábio Albuquerque <albuquerquefabio@icloud.com>
    Date:   Sat Mar 22 02:53:38 2025 -0300
    
        Add Shortener component with styling and functionality for URL shortening
    
    commit 777ca927cc409345ab6b28524ab27cc592f5eafd
    Author: Fábio Albuquerque <albuquerquefabio@icloud.com>
    Date:   Sat Mar 22 02:50:54 2025 -0300
    
        Refactor app.tsx to reintroduce Shortener and Redirect components
    
    commit 19e3a25d8927e6879c4e2bd87e43c119e6068f54
    Merge: 46fee9f 78432f0
    Author: Fábio Albuquerque <albuquerquefabio@icloud.com>
    Date:   Sat Mar 22 02:08:30 2025 -0300
    
        Merge branch 'release/1.1.0' into develop

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,10 +1,7 @@
 import styles from './app.module.scss';
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-
-import { Shortener } from '../shorterner/Shortener';
 import { Flex, Layout, ConfigProvider, theme as antdTheme } from 'antd';
-import { Redirect } from '../redirect/Redirect';
 import { useTheme, ThemeSwitcher } from '../theme-switcher/theme-switcher'; // Import new module
 import { ApiProvider } from '../context/ApiContext';
 import { UserProvider } from '../context/UserContext';
@@ -14,6 +11,8 @@ import Login from '../components/login/Login';
 import Signup from '../components/signup/Signup';
 import { NavBar } from '../components/navbar/NavBar';
 import Panel from '../components/panel/Panel';
+import { Redirect } from '../redirect/Redirect';
+import { Shortener } from '../shortener/shortener';
 
 const { Content } = Layout;
 
